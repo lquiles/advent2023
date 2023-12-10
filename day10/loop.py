5331196af477935980810e71bde5c3298baffe38
Luis Quiles <luis.quiles.jr@gmail.com>
2023-12-10 00:08:55 -0800

    Day 10 Part 1 - walk paths and find furthest pipe

--- day10/loop.py
+++ day10/loop.py
@@ -1,3 +1,5 @@
+from queue import Queue
+
 def init():
   # file = open('day10/sample1.txt', 'r')
   # file = open('day10/sample2.txt', 'r')
@@ -10,9 +12,49 @@ def init():
 
   return (pipes, startY, startX)
 
+def solve(pipes, startY, startX):
+  s = set([(startY, startX)])
+  q = Queue()
+
+  down = ['7', 'F', '|']
+  right = ['F', 'L', '-']
+  up = ['J', 'L', '|']
+  left = ['7', 'J', '-']
+
+  setup = [
+    (startY - 1, startX, down),
+    (startY, startX - 1, right),
+    (startY + 1, startX, up),
+    (startY, startX + 1, left)
+  ]
+
+  for y,x,v in setup:
+    if pipes[y][x] in v:
+      q.put((y, x, 1))
+
+  steps = 1
+  while not q.empty():
+    y,x,i = q.get()
+    s.add((y, x))
+    steps = max(steps, i)
+
+    if pipes[y][x] in up    and pipes[y - 1][x] in down  and (y - 1, x) not in s:
+      q.put((y - 1, x, i + 1))
+    if pipes[y][x] in left  and pipes[y][x - 1] in right and (y, x - 1) not in s:
+      q.put((y, x - 1, i + 1))
+    if pipes[y][x] in down  and pipes[y + 1][x] in up    and (y + 1, x) not in s:
+      q.put((y + 1, x, i + 1))
+    if pipes[y][x] in right and pipes[y][x + 1] in left  and (y, x + 1) not in s:
+      q.put((y, x + 1, i + 1))
+      
+  return steps
+
+
 def main():
   (pipes, startY, startX) = init()
-  print(startY, startX)
+  print((startY, startX))
+  steps = solve(pipes, startY, startX)
+  print(steps)
 
 
 if __name__ == "__main__":
