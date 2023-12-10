7b91b3686918ab4e4a550af81b0a7b98beae64e6
Luis Quiles <luis.quiles.jr@gmail.com>
2023-12-10 01:46:50 -0800

    Day 10 Part 2 - Detect inside of a loop

--- day10/loop.py
+++ day10/loop.py
@@ -3,6 +3,8 @@ from queue import Queue
 def init():
   # file = open('day10/sample1.txt', 'r')
   # file = open('day10/sample2.txt', 'r')
+  # file = open('day10/sample3.txt', 'r')
+  # file = open('day10/sample4.txt', 'r')
   file = open('day10/input.txt', 'r')
 
   pipes = [['.'] + list(line.strip()) + ['.'] for line in file.readlines()]
@@ -11,6 +13,23 @@ def init():
   startY,startX = [(y,line.index('S')) for y,line in enumerate(pipes) if 'S' in line][0]
 
   return (pipes, startY, startX)
+
+def startSymbol(value):
+  # ULDR = 1248
+  # UL.. =  3 = J
+  # U.D. =  5 = |
+  # .LD. =  6 = 7
+  # U..R =  9 = L
+  # .L.R = 10 = -
+  # ..DR = 12 = F
+  return {
+    3:  'J',
+    5:  '|',
+    6:  '7',
+    9:  'L',
+    10: '-',
+    12: 'F'
+  }[value]
 
 def solve(pipes, startY, startX):
   s = set([(startY, startX)])
@@ -28,9 +47,15 @@ def solve(pipes, startY, startX):
     (startY, startX + 1, left)
   ]
 
-  for y,x,v in setup:
+  val = 0
+  for i,r in enumerate(setup):
+    y,x,v = r
+    
     if pipes[y][x] in v:
+      val += 2**i
       q.put((y, x, 1))
+  
+  pipes[startY][startX] = startSymbol(val)
 
   steps = 1
   while not q.empty():
@@ -47,14 +72,33 @@ def solve(pipes, startY, startX):
     if pipes[y][x] in right and pipes[y][x + 1] in left  and (y, x + 1) not in s:
       q.put((y, x + 1, i + 1))
       
-  return steps
+  enclosed = 0
+  for y,line in enumerate(pipes):
+    inside = False
+    corner = '.'
+    for x,c in enumerate(line):
+      if (y,x) in s:
+        if c in ['F','L']:
+          corner = c
+
+        # Treat F--7 as - and F--J as |
+        # Horizontally, a space is inside for odd number of crossing |
+        if c == '|' or (c in down and corner in up) or (c in up and corner in down):
+          inside = not inside
+          corner = '.'
+        continue
+
+      if inside:
+        enclosed += 1
+
+  return (steps, enclosed)
 
 
 def main():
   (pipes, startY, startX) = init()
   print((startY, startX))
-  steps = solve(pipes, startY, startX)
-  print(steps)
+  steps,enclosed = solve(pipes, startY, startX)
+  print((steps, enclosed))
 
 
 if __name__ == "__main__":
