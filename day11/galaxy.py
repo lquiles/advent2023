a6763fd79cad13c73389976fc52d73cfcb75b3f9
Luis Quiles <luis.quiles.jr@gmail.com>
2023-12-11 22:07:00 -0800

    Day 11 Part 2 - Scale for expanding space

--- day11/galaxy.py
+++ day11/galaxy.py
@@ -25,19 +25,20 @@ def distance(pointsInAxis, point1, point2):
   path = set(range(min(point1,point2), max(point1,point2)))
   # Expanded distance = Normal distance - Path intersection
   expandedDistance = distance - len(pointsInAxis & path)
-  
-  return distance + expandedDistance
+
+  scale = int(1E6-1)
+  return (distance + expandedDistance, distance + (scale * expandedDistance))
 
 
 def solve(galaxies, ys, xs):
-  total = 0
+  total = (0, 0)
   for i in range(0, len(galaxies) - 1):
     for j in range(i + 1, len(galaxies)):
       y1,x1 = galaxies[i]
       y2,x2 = galaxies[j]
-      dy = distance(ys,y1,y2) 
-      dx = distance(xs,x1,x2)
-      total += dy + dx
+      dy,ey = distance(ys,y1,y2) 
+      dx,ex = distance(xs,x1,x2)
+      total = (total[0] + dy + dx, total[1] + ey + ex)
   return total
 
 
