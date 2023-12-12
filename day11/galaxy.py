97b430807d27a8a1ea0846e11eb9a838d424cc46
Luis Quiles <luis.quiles.jr@gmail.com>
2023-12-11 21:55:19 -0800

    Day 11 Part 1 - computing distance between galaxies

---
+++ day11/galaxy.py
@@ -0,0 +1,54 @@
+def init(file):
+  # (y,x) Location of every galaxy
+  galaxies = []
+  # All known y coordinates
+  ys = set()
+  # All known x coordinates
+  xs = set()
+
+  for y,line in enumerate([line.strip() for line in file.readlines()]):
+    for x,cell in enumerate(list(line)):
+      if cell == '#':
+        galaxies.append((y,x))
+        ys.add(y)
+        xs.add(x)
+  return (galaxies, ys, xs)
+
+# For a given dimension, compute the expanded travel distance
+# pointsInAxis - array of points from all coordinates in one dimension
+# point1 - value of the first coordinate in one dimension
+# point2 - value of the second coordinate in one dimension
+def distance(pointsInAxis, point1, point2):
+  # Normal distance
+  distance = abs(point1 - point2)
+  # Intersect all known values of this dimension with the path
+  path = set(range(min(point1,point2), max(point1,point2)))
+  # Expanded distance = Normal distance - Path intersection
+  expandedDistance = distance - len(pointsInAxis & path)
+  
+  return distance + expandedDistance
+
+
+def solve(galaxies, ys, xs):
+  total = 0
+  for i in range(0, len(galaxies) - 1):
+    for j in range(i + 1, len(galaxies)):
+      y1,x1 = galaxies[i]
+      y2,x2 = galaxies[j]
+      dy = distance(ys,y1,y2) 
+      dx = distance(xs,x1,x2)
+      total += dy + dx
+  return total
+
+
+def main():
+#   file = open('day11/sample.txt', 'r')
+  file = open('day11/input.txt', 'r')
+  galaxies,ys,xs = init(file)
+  total = solve(galaxies,ys,xs)
+  print(total)
+
+
+
+if __name__ == "__main__":
+  main()
